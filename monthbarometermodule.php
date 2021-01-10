5297cd2045a588b0267458e9a39e0b2a26b0feed
brian underdown <brian260865@gmail.com>
2021-01-10 19:55:41 +0300

    mmHg fixes

--- monthbarometermodule.php
+++ monthbarometermodule.php
@@ -2,9 +2,9 @@
  <?php include('livedata.php');?>
  <div class="modulecaptionchart">Barometer &nbsp;<blue1>
  <?php 
- if ($weather["temp_units"]=='C' && $position8=='barometermod-mmHG.php') {echo 'mmHG';}
+ if ($weather["temp_units"]=='C' && $position8=='barometer-modmmHG.php') {echo 'mmHG';}
  else  echo $weather["barometer_units"]; 
- if ($weather["temp_units"]=='C' && $position8=='barometermod-mmHG.php') {$conv=0.750062;}
+ if ($weather["temp_units"]=='C' && $position8=='barometer-modmmHG.php') {$conv=0.750062;}
  else  $conv=1;
  ?></blue1></div> 
  <iframe  class="charttempmodule" src="weather34charts/monthbarometermodulechart2.php" frameborder="0" scrolling="no" width="320px" height="250px"></iframe>  
--- yearbarometermodule.php
+++ yearbarometermodule.php
@@ -3,9 +3,9 @@
  <div class="modulecaptionchart">Barometer&nbsp;<blue1>
      
  <?php 
- if ($weather["temp_units"]=='C' && $position8=='barometermod-mmHG.php') {echo 'mmHG';}
+ if ($weather["temp_units"]=='C' && $position8=='barometer-modmmHG.php') {echo 'mmHG';}
  else  echo $weather["barometer_units"]; 
- if ($weather["temp_units"]=='C' && $position8=='barometermod-mmHG.php') {$conv=0.750062;}
+ if ($weather["temp_units"]=='C' && $position8=='barometer-modmmHG.php') {$conv=0.750062;}
  else  $conv=1;
  ?>
  
