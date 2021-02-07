53fc5e1b06df241669355b3e2d85cc41b9b35588
brian underdown <brian260865@gmail.com>
2021-02-07 16:20:04 +0300

    Update weather34-lightning-charts.php

--- weather34-lightning-charts.php
+++ weather34-lightning-charts.php
@@ -7,7 +7,7 @@
 	# 	   revised December 2020                                                                    
 	#   https://www.weather34.com 	                                                                   
 	########################################################################
-//original weather34 script original css/svg/php by weather34 2015-2019 clearly marked as original by weather34//
+
 include('livedata.php');
   ?>
   
