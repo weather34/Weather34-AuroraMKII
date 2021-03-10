848fde825839bbc1a15cc708641a91663f0a6953
brian underdown <brian260865@gmail.com>
2021-03-10 19:59:42 +0300

    do not use this file for updating

--- console-settings.php
+++ console-settings.php
@@ -3,12 +3,12 @@ $unit = "english";
 $metric = false;
 $windunit = "mph";
 $windunitatc = "no";
-$distanceunit = "mi";
+$distanceunit = "ft";
 $cloudbaseunit = "mt";
 $tempunit = "F";
 $rainunit  = "in";
 $rainrate = "/h";
-$pressureunit  = "mmHg";
+$pressureunit  = "inHg";
 $livedataFormat = "meteobridge-api";
 $livedata   = "mbridge/MBrealtimeupload.txt";
 $dateFormat   = "d-m-Y";
@@ -17,7 +17,7 @@ $display2019    = "yes";
 $display2020    = "yes";
 $smarttv    = "yes";
 $notifications    = "yes";
-$webcamdevice    = "yes";
+$webcamdevice    = "no";
 $indooricon    = "yes";
 
 $position1    = "temperaturemod.php";
@@ -30,9 +30,9 @@ $position7    = "weather34-rain.php";
 $position8    = "barometer-modmmHG.php";
 $position9    = "barometer-mod.php";
 
-$position10    = "weather34-airquality.php";
-$position11    = "weather34-lightning-chuck.php";
-$position12    = "weather34-airquality-davis.php"; 
+$position10    = "weather34-sunmod.php";
+$position11    = "weather34-moonphasemod.php";
+$position12    = "weather34-firerisk.php"; 
 $position13    = "weather34-airquality-mod.php";
 
 $position10a    = "weather34-dayforecast.php";
@@ -42,20 +42,20 @@ $position12a    = "weather34-indoormod.php";
 
 $stationName   = "Demo PHP 8";
 $theme   = "dark";
-$TZ = "Europe/Berlin";
+$TZ = "Europe/London";
 $lon = 0.1;
 $lat = 50.1;
 $UTC = "0";
 $clockformat    = "12";
 $hemisphere   = "0";
-$webcamurl   = "https://admin.meteobridge.com/cam/07909db1e64c420d95e12dcaa7129a39/camplus.jpg";
+$webcamurl   = "";
 $displayalmanac    = "yes";
 
 $displayukicon    = "no";
-$displaymsicon    = "yes";
+$displaymsicon    = "no";
 $displayktsicon    = "yes";
 
-$uvsensor    = "yes";
+$uvsensor    = "no";
 $mbyear    = "2014";
 $mbplatform   = "NanoSD";
 $brand  = "yes";
@@ -65,11 +65,11 @@ $ifttt  = "no";
 $wuapikey = "123456789xxxxxxx";
 $wuapiunit   = "e";
 $purpleairID = "1218";
-$purpleairhardware   = "yes";
+$purpleairhardware   = "no";
 $purpleairqualitylocation = "outside";
 
 $luftdatenID = "50525";
-$luftdatenhardware   = "no";
+$luftdatenhardware   = "yes";
 $lufdatenairqualitylocation = "outside";
 
 $davisairquality   = "yes";
@@ -80,5 +80,3 @@ $displayemail   = "yes";
 $metarapikey ="5d55a451e66847249ba1a03135";
 $metar   = "yes";
 $icao1   = "LTBA";
-$fontsize   = "24";
-$fontweight   = "yes";
