56ad7b0095353bcfeef7c0589dd329db09e4743a
brian underdown <brian260865@gmail.com>
2021-06-20 17:28:21 +0300

    Ref Josep icons

--- weather34-tv.php
+++ weather34-tv.php
@@ -1,15 +1,11 @@
 <?php 
-  ####################################################################################################
-	#	CREATED FOR WEATHER34 AURORA MKII TEMPLATE 											                           #
-	# https://weather34.com/homeweatherstation/index.html 											                       # 
-	# 	                                                                                               #
-	# 	Release: Feb 2020 TV Vesion  			         #
-	# 	  Console Version                                                                              #
-	#   https://www.weather34.com 	                                                                   #
-	####################################################################################################
-include_once('livedata.php');include_once('updater2.php');
-?>
-
+	########################################################
+	#	CREATED FOR WEATHER34 Aurora TEMPLATE  		              						                
+	# https://weather34.com/homeweatherstation/index.html 											                        
+	# 	                                                                                               
+	# 	Release: December 2020 Version Aurora MKII 				  	                                           
+	########################################################
+include_once('livedata.php');include_once('updater2.php');?>
 <!DOCTYPE html><html><head>
 <title><?php echo $stationName;?> </title>
 <meta name="title" content="<?php echo $stationName;?>">
@@ -28,25 +24,22 @@ include_once('livedata.php');include_once('updater2.php');
 <meta name="apple-mobile-web-app-title" content="Weather34">
 <meta name="application-name" content="Weather34">
 <link href="weather34-theme.css?version=<?php echo filemtime('weather34-theme.css')?>" rel="stylesheet prefetch">
-<link rel="preload" href="fonts/clock3-webfont.woff2" as="font" type="font/woff2" crossorigin>
 <link rel="preload" href="fonts/verbatim-regular.woff2" as="font" type="font/woff2" crossorigin>
 <link rel="preload" href="fonts/verbatim-medium.woff2" as="font" type="font/woff2" crossorigin>
 <link rel="preload" href="fonts/HelveticaNeue-Medium.woff2" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="fonts/verbatim-bold.woff2" as="font" type="font/woff2" crossorigin> 
+
 </head>
 <body>
-<!-- weather34 console flex layout -->
+<!-- weather34 Aurora MKII flex layout -->
 <div class="weather34-tablet">
 <div class="container">
 <div class="nav-top"> 
 <div class="weather34-indoor">
-<?php echo $timeicon?> <div id="weather34clock4"></div>
-<div class="sunriset-set">
-<?php echo $headerlocation?> <?php echo $stationName?></div>
-</div>
+<?php echo $timeicon?><div id="weather34clock4"></div>
+<div class="sunriset-set"><?php echo $headerlocation?> <?php echo $stationName?></div></div>
+<div class="desktoplink2"><div id=data-updated></div></div></div>
 
-<div class="desktoplink3"><div id=data-updated></div></div>
-
-</div>
 <ul class="grid-container">
     <li><div id=position1></div></li>
     <li><div id=position2></div></li>
@@ -60,7 +53,7 @@ include_once('livedata.php');include_once('updater2.php');
     <li2><div id=position10></div></li2> 
     <li2><div id=position11></div></li2> 
     <li2><div id=position12></div></li2>  
-    <li3><div id=position13></div></li3>
+    <li3><div id=position13></div></li3>    
   </ul>
 
   <div class="nav-bottom">  
@@ -68,9 +61,8 @@ include_once('livedata.php');include_once('updater2.php');
  <?php //include custom weather34 menu file 
  include('weather34-sidemenu.php');?>
  </div></div>
- <div class="weather34-open-menu"><button><?php echo $adjust ?> <icontext>Menu</icontext></button></div>
+ <div class="weather34-open-menu"><button><?php echo $adjust ?> <icontext>Menu</icontext></button></div> 
  <div class="weather34switcher">
-<div class="weather34switcher">
  <a data-title="Switch Theme"><button id="weather34theme-toggle" class="weather34switch" type="button"> <?php echo $themeshadelight;?> 
 </button></div></a>
 <script>
@@ -87,9 +79,10 @@ toggle.onclick = function() {var currentTheme = document.documentElement.getAttr
 </script>
 <bottomtoolbarspace></bottomtoolbarspace>
 
+
      <?php 
   if ($units=='us') {  // NON METRIC OPTIONS C-KTS-MS-UK  
-    echo '<a  href="?units=metric" data-title="Metric Units">
+    echo '<a  href="?units=metric" data-title="Metric Units" >
     '.$weather34C.'</a>'; 
 
     if ($displaymsicon=='yes'){ 
@@ -97,12 +90,8 @@ toggle.onclick = function() {var currentTheme = document.documentElement.getAttr
     '.$weather34MS.'</a>'; }
 
     if ($displayukicon=='yes'){ 
-      echo '<a href="?units=uk" data-title="UK Units"> 
-      '.$weather34UK.'</a>'; } 
-
-    if ($displayktsicon=='yes'){ 
-    echo '<a  href="?units=knots" data-title="Wind Knots">
-    '.$weather34KTS.'</a>';  }   
+      echo '<a href="?units=uk" data-title="UK Units" class="footericons"> 
+      '.$weather34UK.'</a>'; }     
   }
   else if ($units=='uk'){ // UK OPTIONS F-C-KNOTS
     echo '<a  href="?units=us" data-title="Imperial Units">
@@ -110,91 +99,61 @@ toggle.onclick = function() {var currentTheme = document.documentElement.getAttr
      echo '<a  href="?units=metric" data-title="Metric Units">
      '.$weather34C.'</a>';
      if ($displayktsicon=='yes'){ 
-    echo '<a  href="?units=knots" data-title="Wind Knots">
+    echo '<a  href="?units=knots" data-title="Wind Knots" class="footericons">
     '.$weather34KTS.'</a>';   }  
   }  
   else if ($units=='metric'){ // METRIC OPTIONS F-UK-KTS
     echo '<a  href="?units=us" data-title="Imperial Units">
     '.$weather34F.'</a>';  
     if ($displayukicon=='yes'){ 
-     echo '<a href="?units=uk" data-title="UK Units"> 
+     echo '<a href="?units=uk" data-title="UK Units" class="footericons"> 
      '.$weather34UK.'</a>'; } 
      if ($displayktsicon=='yes'){ 
-     echo '<a  href="?units=knots" data-title="Wind Knots">
+     echo '<a  href="?units=knots" data-title="Wind Knots" >
      '.$weather34KTS.'</a>'; }    
   }  
 
   else if ($units=='scandinavia'){ // MS OPTIONS F-C-KTS
-    echo '<a  href="?units=us" data-title="Imperial Units">
+    echo '<a  href="?units=us" data-title="Imperial Units" class="footericons">
     '.$weather34F.'</a>'; 
     echo '<a  href="?units=metric" data-title="Metric Units">
-    '.$weather34C.'</a>';  
-  if ($displayktsicon=='yes'){     
-    echo '<a  href="?units=knots" data-title="Wind Knots">
-    '.$weather34KTS.'</a>'; }      
+    '.$weather34C.'</a>';     
   }  
   else if ($units=='knots'){   // KNOTS OPTIONS F-C-UK
-    echo '<a  href="?units=us" data-title="Imperial Units">
+    echo '<a  href="?units=us" data-title="Imperial Units" class="footericons">
     '.$weather34F.'</a>';  
-    echo '<a  href="?units=metric" data-title="Metric Units">
+    echo '<a  href="?units=metric" data-title="Metric Units" >
     '.$weather34C.'</a>'; 
-    if ($displayukicon=='yes'){ 
-    echo '<a href="?units=uk" data-title="UK Units">     
-    '.$weather34UK.'</a>';  } 
-   } 
+      } 
    
    else if ($units==''){   // default
     echo '<a  href="?units=us" data-title="Imperial Units">
     '.$weather34F.'</a>';  
     echo '<a  href="?units=metric" data-title="Metric Units">
-    '.$weather34C.'</a>'; 
-    if ($displayukicon=='yes'){ 
-    echo '<a href="?units=uk" data-title="UK Units">     
-    '.$weather34UK.'</a>'; }  
+    '.$weather34C.'</a>';   
    } 
 ?>
 
-<a href="outlookwu.php" data-lity data-title="5 day Forecast">
-<icon-forecast><?php echo $weather34fore?></icon-forecast></a> 
-
-<a class="footericons" href="outlookwutext.php" data-lity  data-title="Summary Forecast">
-<?php echo $weather34foretxt?></a>
-
-<a href="consolecharts.php" data-title="Daily Charts">
-<?php echo $weather34chart2?></a>
-
-<a href="weather34-almanac.php" data-title="Almanac">
-<?php echo $weather34alm?></a>
-
-<?php if ($indooricon == "yes") {?> 
-<a  href="weather34-ev.php"  data-title="Indoor Enviroment"> <?php echo $hometempmenu;?></a>
-<?php };?>
-
+<a href="forecastcharts/chartforecast.php" data-title="5 day Forecast" ><icon-forecast><?php echo $weather34fore?></icon-forecast></a> 
+<a class="footericons" href="outlookwutext.php" data-lity  data-title="Summary Forecast"><?php echo $weather34foretxt?></a>
+<a href="consolecharts.php" data-title="Daily Charts"><?php echo $weather34chart2?></a>
+<a class="footericons" href="weather34-almanac.php" data-title="Almanac"><?php echo $weather34alm?></a>
+<a href="index.php" data-title="Refresh" class="showicon2"><?php echo $weather34refr?></a>
 <weather34-rightfootericons>
-<?php if ($webcamdevice == "yes") {?>
-  <a href="weather34-large-cam.php" data-lity data-title="Webcam"><?php echo $webcamicon2?></a>
-<?php };?>
+<aurora>Aurora MKII (SE) <?php echo $weather["stationtype"]?></aurora>
 
-<?php if ($purpleairhardware == "yes") {?>
-  <a href="weather34-aqi-info.php" data-lity data-title="Air Quality"><?php echo $weather34pa?></a>
-<?php };?>
+<a href="index.php" data-title="Desktop Version" class="showicon3" > <?php echo $weather34desktop?></a>
 
-<?php if ($luftdatenhardware=='yes') {?>
-  <a href="weather34-aqi-info-luftdaten.php" data-lity data-title="Air Quality"><?php echo $weather34aqi?></a>
-<?php };?>
-<a  href="weather34-template-legend.php" data-lity data-title="Hardware Info" class="footericons"> <?php echo $weather34hinfo;?></a>
-</portraitmode>
-<a href="index.php" data-title="Desktop Version"> <?php echo $weather34desktop?></a>
 
-<?php if ($brand=='yes') {?>
-<div class="weather34-battery">
-  <a  href="info.html" data-lity data-title="Weather34"> <img src="images/weather34-icon-small.png" width="24em" ></a></div>  
-  
-<?php };?></div> 
+<div class="weather34-aurora" ><a  href="info.html" data-lity data-title="Weather34" class="footericons"> 
+<img src="images/weather34-logo-small.svg" width="23px" height="23px" ></a></div>
 
+</weather34-rightfootericons>
+</div>
 <?php //if notifications is yes weather34 rain/snow/thunder awareness every 15 minutes 
 if ($notifications=='yes') {?><div id=forecastalert></div><?php ;}?>
+</body>
 
 <script>//weather menu sidebar
 $(function() {var box = $('.weather34-menuside');var button = $('.weather34-open-menu, .weather34-header-menu');button.on('click', function(){box.toggleClass('weather34-highlight');});});</script> 
-</html></body>
+</html>
