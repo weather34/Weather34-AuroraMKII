5969ae6d4feac218665ad8973efd03c8110034e5
brian underdown <brian260865@gmail.com>
2021-06-25 17:51:34 +0300

    Ref Alex for mmHG screen

--- barometer-almanacmmHg2.php
+++ barometer-almanacmmHg2.php
@@ -14,51 +14,54 @@ $weather["barometer_min"]=number_format($meteobridgeapi[36]*0.75006157584566,1);
 
 ?>
 <link href="weather34-theme.css?version=<?php echo filemtime('weather34-theme.css') ?>" rel="stylesheet prefetch">
-<theword>Barometric Pressure (<blue><?php echo $weather["barometer_units"];?></blue>)</theword>
-<extrainfoicon><?php echo $weather34_pressure_icon;?></extrainfoicon>
-<div class="weather34credit">
-<a class="canvascreditlink" href="https://canvasjs.com" target="_blank" data-title="CanvasJs.com" >
-Charts compiled with CanvasJs.com <br>v2.3.1 GA (CC BY-NC 3.0) Non-Commercial-Version.</a></div></div>
+<ul class="grid-containeralmcharts">
+<li><iframe  class="charttempmodule3"  src="weather34charts/todaybarometermodulechart2.php" frameborder="0" scrolling="no" ></iframe></li>
+<li><iframe  class="charttempmodule3"  src="weather34charts/monthbarometermodulechart2.php" frameborder="0" scrolling="no" ></iframe>  </li>
+<li><iframe  class="charttempmodule3"  src="weather34charts/yearbarometermodulechart2.php" frameborder="0" scrolling="no" ></iframe>  </li>
+<li>
+<div style="margin-top:10px;">
+<alamanacword>Barometer History Data</alamanacword>
 
-
-<div class="almanacouterboxrain">
-<br><br>
-<div class="almanacchartx">
-<monthchart>Current Day Barometer Chart</monthchart>
-<iframe  class="charttempmodule" src="weather34charts/todaybarometermodulechart2.php" frameborder="0" scrolling="no" width="320px" height="200px"></iframe>  
-</div>
-<div class="almanacx"><div class="almanac-content">
+<div class="alamanacdata">
 <?php  //month max
-echo "<valuetextheading1>".date('F')." Max <deepblue>".$weather["thb0seapressmonthmaxtime"]."</deepblue></valuetextheading1><br>";   
-echo "<div class=almanacareas>".$weather["thb0seapressmmax"]."<smalltempunit2>".$weather["barometer_units"] ;
-?><smalltempunit2></div></div>
-
-<div class="almanac2x"><div class="almanac-content">
-<?php  //max year
-echo "<valuetextheading1>".date('Y')." Max <deepblue>".$weather["thb0seapressyearmaxtime"]."</deepblue></valuetextheading1><br>";
-echo "<div class=almanacareas>".$weather["thb0seapressymax"]."<smalltempunit2>".$weather["barometer_units"] ;
- ?></smalltempunit2></div></div>
-
-<div class="almanac3x"><div class="almanac-content">
+echo "".date('F')." <orange>Max</orange> ".$weather["thb0seapressmonthmaxtime"];
+echo " ".$weather["thb0seapressmmax"]."".$weather["barometer_units"] ;
+?></div>
+<div class="alamanacdata">
 <?php  //month min
-echo "<valuetextheading1>".date('F')." Min <deepblue>".$weather["thb0seapressmonthmintime"]."</deepblue></valuetextheading1><br>";
-echo "<div class=almanacareas>".$weather["thb0seapressmmin"]."<smalltempunit2>".$weather["barometer_units"] ;
-?><smalltempunit2></div></div>
+echo "".date('F')." <deepblue>Min</deepblue> ".$weather["thb0seapressmonthmintime"];
+echo " ".$weather["thb0seapressmmin"]."".$weather["barometer_units"] ;
+?></div>
 
-<div class="almanac4x"><div class="almanac-content">
-<?php  //min Year
-echo "<valuetextheading1>".date('Y')." Min <deepblue>".$weather["thb0seapressyearmintime"]."</deepblue></valuetextheading1><br>";
-echo "<div class=almanacareas>".$weather["thb0seapressymin"]."<smalltempunit2>".$weather["barometer_units"] ;
-?><smalltempunit2></div></div>
+<div class="alamanacdata">
+<?php  //year max
+echo "".date('F')." <orange>Max</orange> ".$weather["thb0seapressyearmaxtime"];
+echo " ".$weather["thb0seapressymax"]."".$weather["barometer_units"] ;
+?></div>
+<div class="alamanacdata">
+<?php  //year min
+echo "".date('Y')."  <deepblue>Min</deepblue> ".$weather["thb0seapressyearmintime"];
+echo " ".$weather["thb0seapressymin"]."".$weather["barometer_units"] ;
+?></div>
 
-<div class="almanac5x"><div class="almanac-content">
+<div class="alamanacdata">
 <?php  //alltime max
-echo "<valuetextheading1>Record Max <deepblue>".$weather["thb0seapressamaxtime"]."</deepblue></valuetextheading1><br>";
-echo "<div class=almanacareas>".$weather["thb0seapressamax"]."<smalltempunit2>".$weather["barometer_units"] ;
-?><smalltempunit2></div></div>
+echo "Alltime <orange>Max</orange> ".$weather["thb0seapressamaxtime"];
+echo " ".$weather["thb0seapressamax"]."".$weather["barometer_units"] ;
+?></div>
 
-<div class="almanac6x"><div class="almanac-content">
+<div class="alamanacdata">
 <?php  //alltime min
-echo "<valuetextheading1>Record Min <deepblue>".$weather["thb0seapressamintime"]."</deepblue></valuetextheading1><br>";
-echo "<div class=almanacareas>".$weather["thb0seapressamin"]."<smalltempunit2>".$weather["barometer_units"] ;
-?><smalltempunit2></div></div></div>
+echo "Alltime <deepblue>Min</deepblue> ".$weather["thb0seapressamintime"];
+echo " ".$weather["thb0seapressamin"]."".$weather["barometer_units"] ;
+?></div>
+
+
+
+</div>
+</li>
+</ul>
+</div></div>
+<weather34credit>
+<a href="https://canvasjs.com" target="_blank" data-title="https://canvasjs.com" >
+CanvasJs.com v2.3.1 GA (CC BY-NC 3.0) Non-Commercial-Version</a></weather34credit>
