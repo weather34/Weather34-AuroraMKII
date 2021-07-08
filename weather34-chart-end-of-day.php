51631dbd58ff15ed252f5438f8bff0afddd2c937
brian underdown <brian260865@gmail.com>
2021-07-08 17:56:04 +0300

    adds forthcoming month/year chart data humidity ref Josep

--- weather34-chart-end-of-day.php
+++ weather34-chart-end-of-day.php
@@ -2,11 +2,13 @@
     #######################################################
 	#	CREATED FOR Aurora MKII version								   							   
 	# https://weather34.com/homeweatherstation/index.html 	
-	# 	     Creates and adds month and year  csv data  
-	# 	Updated Release: Revised January 2021		
+	# 	     Creates and adds month and year csv data  
+	# 	Updated Release: Revised July 2021		
 	# 	                                                                                               
 	#   https://www.weather34.com 	                                                                   
 	#######################################################
+
+//position numbers for charts
 //0 (date-month-0)),
 //1 MAX(yesterday Temp-1-$meteobridgeapi[82]) 
 //2 MIN(yesterday Temp-2-$meteobridgeapi[84])
@@ -15,7 +17,7 @@
 // 5 TOTAL(yesterday Rain total-5-$meteobridgeapi[100])
 // 6 MAX(yesterday Windspeed-6-$meteobridgeapi[94])
 // 7 AVG(yesterday WindSpeed-7-$meteobridgeapi[123])
-// 8 not currently used $meteobridgeapi[--] * reserved 
+// 8 not currently used $meteobridgeapi[--] * reserved for future 
 // 9 MAX(yesterday Barometer-9-$meteobridgeapi[135])
 // 10 MIN(yesterday Barometer-10-$meteobridgeapi[137])
 //11 Max( yesterday UV-11-$meteobridgeapi[114])
@@ -23,6 +25,8 @@
 //13 Purple Air(13-$aqiweather["aqi24h"]) *if yes 
 //14 AIRQ 24H(14-$meteobridgeapi[184]) * Davis Airlink 
 //15 yesterday Wind DIRECTION average (15-$meteobridgeapi[171])
+//16 yesterday Max Humidity (16-$meteobridgeapi[167])
+//17 yesterday Min Humidity (17-$meteobridgeapi[169])
 
 date_default_timezone_set($TZ);
 $date = date_create();
@@ -40,17 +44,16 @@ $aqiweather["aqi24h"]      = number_format(pm25_to_aqi(($sensor24h )),1);}
 $meteobridgeapi[184] = number_format(pm25_to_aqi(($meteobridgeapi[184])),1);
 
 //create the current month file example folder/file path = weather34charts/2021/July.csv
-
-// [DD][MM] , [th0temp-ydmax:--] , [th0temp-ydmin:--] , [th0dew-ydmax:--] , [th0dew-ydmin:--] , [rain0total-ydmax:--] , [wind0wind-ydmax:--] , [wind0wind-ydavg:—] , [position 8 not used] ,  [thb0seapress-ydmax:--] , [thb0seapress-ydmin:--] ,  [uv0index-ydmax:--] , [sol0rad-ydmax:--] , [air0pm-ydavg:—] , [air1pm-ydavg:—] , [wind0dir-ydavg:--]
+// [DD][MM] , [th0temp-ydmax:--] , [th0temp-ydmin:--] , [th0dew-ydmax:--] , [th0dew-ydmin:--] , [rain0total-ydmax:--] , [wind0wind-ydmax:--] , [wind0wind-ydavg:—] , [position 8 not used] ,  [thb0seapress-ydmax:--] , [thb0seapress-ydmin:--] ,  [uv0index-ydmax:--] , [sol0rad-ydmax:--] , [air0pm-ydavg:—] , [air1pm-ydavg:—] , [wind0dir-ydavg:--] ,$weather["humidity_ydmax"] ,$weather["humidity_ydmin"]
 $weatherchartfilemonth ="weather34charts/".$year."/".date('F').".csv";
-$weather34chartdata = date('j M',strtotime("-1 day")).",".$meteobridgeapi[82].",".$meteobridgeapi[84].",".$meteobridgeapi[52].",".$meteobridgeapi[120].",".$meteobridgeapi[100].",".$meteobridgeapi[94].",".$meteobridgeapi[123].",".$meteobridgeapi[123].",".$meteobridgeapi[135].",".$meteobridgeapi[137].",".$meteobridgeapi[114].",".$meteobridgeapi[107].",".$aqiweather["aqi24h"].",".$meteobridgeapi[184].",".$meteobridgeapi[171].""."\r\n";
+$weather34chartdata = date('j M',strtotime("-1 day")).",".$meteobridgeapi[82].",".$meteobridgeapi[84].",".$meteobridgeapi[52].",".$meteobridgeapi[120].",".$meteobridgeapi[100].",".$meteobridgeapi[94].",".$meteobridgeapi[123].",".$meteobridgeapi[123].",".$meteobridgeapi[135].",".$meteobridgeapi[137].",".$meteobridgeapi[114].",".$meteobridgeapi[107].",".$aqiweather["aqi24h"].",".$meteobridgeapi[184].",".$meteobridgeapi[171].",".$meteobridgeapi[167].",".$meteobridgeapi[169].""."\r\n";
 //$output=$weatherchartfilemonth;
 $fp = fopen($weatherchartfilemonth, 'a+'); 
 fwrite($fp,$weather34chartdata); 
 fclose($fp);
 //create the year file example folder/file path = weather34charts/2021.csv
 $weatherchartfileyear = "weather34charts/".date('Y').".csv";
-$weather34chartdata1  = date('j M',strtotime("-1 day")).",".$meteobridgeapi[82].",".$meteobridgeapi[84].",".$meteobridgeapi[52].",".$meteobridgeapi[120].",".$meteobridgeapi[100].",".$meteobridgeapi[94].",".$meteobridgeapi[123].",".$meteobridgeapi[123].",".$meteobridgeapi[135].",".$meteobridgeapi[137].",".$meteobridgeapi[114].",".$meteobridgeapi[107].",".$aqiweather["aqi24h"].",".$meteobridgeapi[184].",".$meteobridgeapi[171].""."\r\n";
+$weather34chartdata1  = date('j M',strtotime("-1 day")).",".$meteobridgeapi[82].",".$meteobridgeapi[84].",".$meteobridgeapi[52].",".$meteobridgeapi[120].",".$meteobridgeapi[100].",".$meteobridgeapi[94].",".$meteobridgeapi[123].",".$meteobridgeapi[123].",".$meteobridgeapi[135].",".$meteobridgeapi[137].",".$meteobridgeapi[114].",".$meteobridgeapi[107].",".$aqiweather["aqi24h"].",".$meteobridgeapi[184].",".$meteobridgeapi[171].",".$meteobridgeapi[167].",".$meteobridgeapi[169].""."\r\n";
 //$output1=$weatherchartfileyear;
 $fp1 = fopen($weatherchartfileyear, 'a+'); 
 fwrite($fp1,$weather34chartdata1); 
